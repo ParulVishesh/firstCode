1d772d4502dd9fd5f9ee6324e4c5b1a9d6140efe
ParulVishesh <137691274+ParulVishesh@users.noreply.github.com>
2023-06-25 16:18:42 +0530

    Update namedPipe.groovy

--- namedPipe.groovy
+++ namedPipe.groovy
@@ -1,3 +1,33 @@
+#!groovy
+
+env.GIT_URL = 'https://github.com/ParulVishesh/firstCode.git'
+
+node(LABEL) {
+    timestamps {
+        stage('Checkout') {
+                    // TODO: Use the git step when it has implemented specifying refspecs
+                    // See https://jenkins.io/doc/pipeline/steps/workflow-scm-step/ for CloneOption
+                    checkout([$class: 'GitSCM', branches: [[name: ROOT_BRANCH]], doGenerateSubmoduleConfigurations: false,
+                            extensions: [[$class: 'CloneOption', timeout: 10, noTags: true, shallow: false]]
+                                       +[[$class: 'LocalBranch', localBranch: '']]
+                                       +[[$class: 'CleanBeforeCheckout', deleteUntrackedNestedRepositories: true]],
+                            submoduleCfg: [], userRemoteConfigs: [[refspec: ROOT_REFSPEC, url: env.GIT_URL]]])
+        }
+        stage('print output'){
+            sh'''
+                pyhton example.py
+            '''
+    }
+}
+
+
+
+
+
+
+
+
+
 pipeline {
     agent any
     stages {
